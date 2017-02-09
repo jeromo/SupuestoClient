a977bba4392aa290271da7a977121941119a2b05
jrojo <jrojomo@gmail.com>
2017-02-09 12:53:22 +0100

    getIdentifier coded

--- src/users.cpp
+++ src/users.cpp
@@ -2,5 +2,20 @@
 
 Users::Users()
 {
+   for (int i = min_identifier;i <= max_identifier;i++)
+   {
+       vectorIdentifiers.push_back(i);
+   }
+}
+int Users::getIdentifier(unsigned position)
+{
 
+    if (position > vectorIdentifiers.size())
+        return -1;
+
+
+    int value = vectorIdentifiers[position];
+    vectorIdentifiers.erase(vectorIdentifiers.begin() + position);
+
+    return value;
 }
--- src/users.h
+++ src/users.h
@@ -2,10 +2,23 @@
 #define USERS_H
 
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
 class Users
 {
+private:
+    const int max_identifier = 250;
+    const int min_identifier = 1;
+    vector<int> vectorIdentifiers;
+
 public:
     Users();
+    int getIdentifier (unsigned position);
+    string createUserName();
 };
 
 #endif // USERS_H
